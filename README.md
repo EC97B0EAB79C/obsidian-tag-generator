469f465144201967127ecfa0381698837db62c3a
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2025-05-16 11:04:15 +0900

    OpenAI API Baseline

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # obsidian-tag-generator
+
+## Usage
+1. Install dependencies
+    ```bash
+    npm install
+    ```
+2. Build
+    ```bash
+    npm run dev
+    ```
--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
 import { Notice, Plugin, Editor, MarkdownView } from 'obsidian';
+import OpenAI from "openai";
+
+const token = process.env["GITHUB_TOKEN"];
+const endpoint = "https://models.github.ai/inference";
+const model = "openai/gpt-4.1-mini";
 
 export default class MyPlugin extends Plugin {
 
@@ -7,9 +12,25 @@ export default class MyPlugin extends Plugin {
             id: 'generate-tag',
             name: 'Generate Tag',
             // hotkeys: [{ modifiers: ['Mod', 'Shift'], key: 't' }],
-            editorCallback: (editor: Editor, view: MarkdownView) => {
-                new Notice('Generating Tag...');
+            editorCallback: async (editor: Editor, view: MarkdownView) => {
+                // new Notice(`${view.file?.path}`);
+                new Notice('Generating tag...');
+
+                const client = new OpenAI({ baseURL: endpoint, apiKey: token, dangerouslyAllowBrowser: true });
+
+                const response = await client.chat.completions.create({
+                    messages: [
+                        { role: "system", content: "You are a helpful assistant." },
+                        { role: "user", content: "This is a API test" }
+                    ] as Array<{ role: "system" | "user" | "assistant"; content: string }>,
+                    temperature: 1.0,
+                    top_p: 1.0,
+                    model: model
+                });
+
+                new Notice(`Response: ${response.choices[0].message.content}`);
             }
         });
+
     }
 }
